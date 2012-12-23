a706587ba7ade632275821dccb3e3fefb2ad03db
Saad <sbakht@eden.rutgers.edu>
2012-12-23 09:09:33 -0500

    Start and Stop buttons
    cleaned up code, improved functionality of start button, added stop
    button. GPS updates now only start when start button is pressed and it
    finds a valid destination address. Stop button stops vibration from when
    distance <= 1 and stops gps updates.

--- Maps/gen/com/example/maps/R.java
+++ Maps/gen/com/example/maps/R.java
@@ -15,10 +15,11 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int bGo=0x7f070002;
+        public static final int bStart=0x7f070002;
+        public static final int bStop=0x7f070003;
         public static final int etAddress=0x7f070001;
-        public static final int menu_settings=0x7f070004;
-        public static final int mvMain=0x7f070003;
+        public static final int menu_settings=0x7f070005;
+        public static final int mvMain=0x7f070004;
         public static final int tvlatlong=0x7f070000;
     }
     public static final class layout {
--- Maps/src/com/example/maps/MainActivity.java
+++ Maps/src/com/example/maps/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.maps;
 import java.io.IOException;
 import java.util.List;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -13,33 +11,39 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
-import com.google.android.maps.Overlay;
 
+/*
+ * THINGS TO IMRPOVE/CHECK:
+ * won't work if lat/long is exactly 0
+ * should be fine if address is invalid or empty because try/catch?
+ * To VISHAL: add more stuff to these comments as necessary, and PUT COMMENTS EVERYWHERE
+ * Check if vibration and gps location updates turn off after hitting stop
+ * (vibrator isn't set up until distance <=1 so duno what will happen)
+ * What happens if hit stop without starting(should be fine i think)?
+ * Do we want to stop vibration on leaving app? (onPause)
+ * 
+ */
 public class MainActivity extends MapActivity implements View.OnClickListener {
 
 	private LocationManager lm;
 	private LocationListener locationListener;
 
 	MapView map;
-	long start, stop;
 	private MapController mc;
 	TextView latlong;
 	EditText destAddress;
-	Button submit;
+	Button start, stop;
 	double destLat = 0;
 	double destLong = 0;
-	Vibrator v;
+	Vibrator vibrate;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -51,23 +55,15 @@ public class MainActivity extends MapActivity implements View.OnClickListener {
 
 		latlong = (TextView) findViewById(R.id.tvlatlong);
 		destAddress = (EditText) findViewById(R.id.etAddress);
-		submit = (Button) findViewById(R.id.bGo);
-		submit.setOnClickListener(this);
-		//destAddress.setText("30 Allison Drive");
-
-		// ---use the LocationManager class to obtain GPS locations---
-		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-		locationListener = new MyLocationListener();
-
-		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1*60*1000, 0,
-				locationListener); // updates every 1 min
+		start = (Button) findViewById(R.id.bStart);
+		stop = (Button) findViewById(R.id.bStop);
+		start.setOnClickListener(this);
+		stop.setOnClickListener(this);
 
 		// //////////////////////
 		mc = map.getController();
 
 		// String coordinates[] = {"36.487303","-94.308493"};
-
 
 		// double lat = Double.parseDouble(coordinates[0]);
 		// double lng = Double.parseDouble(coordinates[1]);
@@ -81,10 +77,9 @@ public class MainActivity extends MapActivity implements View.OnClickListener {
 		// map.invalidate();
 		// ////////////////////
 
-
-		Touchy t = new Touchy();
-		List<Overlay> overlayList = map.getOverlays();
-		overlayList.add(t);
+		// Touchy t = new Touchy();
+		// List<Overlay> overlayList = map.getOverlays();
+		// overlayList.add(t);
 	}
 
 	@Override
@@ -93,88 +88,43 @@ public class MainActivity extends MapActivity implements View.OnClickListener {
 		return false;
 	}
 
-	class Touchy extends Overlay {
-		@SuppressWarnings("deprecation")
-		public boolean onTouchEvent(MotionEvent e, MapView m) {
-			if (e.getAction() == MotionEvent.ACTION_DOWN) {
-				start = e.getEventTime();
-
-			}
-			if (e.getAction() == MotionEvent.ACTION_UP) {
-				stop = e.getEventTime();
-			}
-			if (stop - start > 1500) {
-				AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
-						.create();
-				alert.setTitle("Pick an Option");
-				alert.setMessage("I told u to pick option");
-				alert.setButton("place a pin",
-						new DialogInterface.OnClickListener() {
-
-							public void onClick(DialogInterface dialog,
-									int which) {
-								// TODO Auto-generated method stub
-
-							}
-						});
-				alert.setButton2("get address",
-						new DialogInterface.OnClickListener() {
-
-							public void onClick(DialogInterface dialog,
-									int which) {
-								// TODO Auto-generated method stub
-
-							}
-						});
-				alert.setButton3("place a pinpoint",
-						new DialogInterface.OnClickListener() {
-
-							public void onClick(DialogInterface dialog,
-									int which) {
-								// TODO Auto-generated method stub
-
-							}
-						});
-				alert.show();
-				return true;
-
-			}
-			return false;
-		}
-	}
-
 	private class MyLocationListener implements LocationListener {
 		public void onLocationChanged(Location loc) {
 			if (loc != null && destLat != 0 && destLong != 0) {
-				//Toast.makeText(
-					//	getBaseContext(),
-						//"Location changed : Lat: " + loc.getLatitude()
-					//			+ " Lng: " + loc.getLongitude(),
-					//	Toast.LENGTH_SHORT).show();
+				// Toast.makeText(
+				// getBaseContext(),
+				// "Location changed : Lat: " + loc.getLatitude()
+				// + " Lng: " + loc.getLongitude(),
+				// Toast.LENGTH_SHORT).show();
 
-				//GeoPoint p = new GeoPoint((int) (loc.getLatitude() * 1E6),
-				//		(int) (loc.getLongitude() * 1E6));
-				//mc.animateTo(p);
-			//	mc.setZoom(16);
-			//	map.invalidate();
+				// GeoPoint p = new GeoPoint((int) (loc.getLatitude() * 1E6),
+				// (int) (loc.getLongitude() * 1E6));
+				// mc.animateTo(p);
+				// mc.setZoom(16);
+				// map.invalidate();
 				double currentLat = loc.getLatitude();
 				double currentLong = loc.getLongitude();
-				double dist = distance(destLat, destLong, currentLat, currentLong);
-				latlong.setText("Distance to destination: " + String.valueOf(dist));
-				
-				if(dist <= 1) {
+				double dist = distance(destLat, destLong, currentLat,
+						currentLong);
+				latlong.setText("Distance to destination: "
+						+ String.valueOf(dist));
+
+				if (dist <= 1) {
 					// Get instance of Vibrator from current Context
-					Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-					 
+					vibrate = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
 					// Start immediately
 					// Vibrate for 200 milliseconds
 					// Sleep for 500 milliseconds
 					long[] pattern = { 0, 200, 500 };
-					 
-					// The "0" means to repeat the pattern starting at the beginning
-					// CUIDADO: If you start at the wrong index (e.g., 1) then your pattern will be off --
-					// You will vibrate for your pause times and pause for your vibrate times !
-					v.vibrate(pattern, 0);
+
+					// The "0" means to repeat the pattern starting at the
+					// beginning
+					// CUIDADO: If you start at the wrong index (e.g., 1) then
+					// your pattern will be off --
+					// You will vibrate for your pause times and pause for your
+					// vibrate times !
+					vibrate.vibrate(pattern, 0);
 
 				}
 			}
@@ -201,13 +151,14 @@ public class MainActivity extends MapActivity implements View.OnClickListener {
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
-		v.cancel(); //stops the vibration alarm
+		vibrate.cancel(); // stops the vibration alarm on leaving app?
 
 	}
 
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.bGo:
+		case R.id.bStart:
+			
 			try {
 				Geocoder coder = new Geocoder(this);
 				List<Address> address;
@@ -234,21 +185,35 @@ public class MainActivity extends MapActivity implements View.OnClickListener {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			finally{
+				// ---use the LocationManager class to obtain GPS locations---
+				lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+				locationListener = new MyLocationListener(); //a location listener to be used below
+
+				lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1*60*1000, 0,
+						locationListener); // updates your location every 1 min
+			}
+			break;
+		case R.id.bStop:
+			vibrate.cancel(); //should stop vibration on stop button
+			lm.removeUpdates(locationListener); //should stop gps updates of locationListener
 			break;
 		}
 	}
-	
-    private double distance(double lat1, double lng1, double lat2, double lng2) {
-        double earthRadius = 3958.75;
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLng = Math.toRadians(lng2-lng1);
-        double sindLat = Math.sin(dLat / 2);
-        double sindLng = Math.sin(dLng / 2);
-        double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
-                * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        double dist = earthRadius * c;
 
-        return dist;
-      }
+	private double distance(double lat1, double lng1, double lat2, double lng2) {
+		double earthRadius = 3958.75;
+		double dLat = Math.toRadians(lat2 - lat1);
+		double dLng = Math.toRadians(lng2 - lng1);
+		double sindLat = Math.sin(dLat / 2);
+		double sindLng = Math.sin(dLng / 2);
+		double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+				* Math.cos(Math.toRadians(lat1))
+				* Math.cos(Math.toRadians(lat2));
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double dist = earthRadius * c;
+
+		return dist;
+	}
 }
