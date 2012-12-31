3bb7d4a0c622d578b960e4723264ba9909568916
Vishal Jain <vkjrul3z@gmail.com>
2012-12-31 03:44:54 -0500

    Reading from internal storage
    makes new empty files in internal storage if no file exists
    reads contents of file and sets up menu
    onclick-> send it away in a bundle to load in mainActivity
    worked on some nested layouts
    nothing tested yet, i'm exhausted

--- Maps/gen/com/example/maps/R.java
+++ Maps/gen/com/example/maps/R.java
@@ -16,16 +16,17 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int aboutUs=0x7f08000a;
+        public static final int aboutUs=0x7f08000b;
         public static final int bEnd=0x7f080007;
         public static final int bStart=0x7f080003;
         public static final int bStop=0x7f080004;
         public static final int etAddress=0x7f080002;
-        public static final int exit=0x7f08000c;
+        public static final int exit=0x7f08000d;
+        public static final int flMenu=0x7f080009;
         public static final int header=0x7f080006;
-        public static final int menu_settings=0x7f080009;
+        public static final int menu_settings=0x7f08000a;
         public static final int mvMain=0x7f080005;
-        public static final int options=0x7f08000b;
+        public static final int options=0x7f08000c;
         public static final int textView1=0x7f080000;
         public static final int title_complex=0x7f080008;
         public static final int tvlatlong=0x7f080001;
--- Maps/src/com/example/maps/Home.java
+++ Maps/src/com/example/maps/Home.java
@@ -1,7 +1,14 @@
 package com.example.maps;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,11 +17,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
-public class Home extends Activity implements OnClickListener{
+public class Home extends ListActivity implements OnClickListener{
 
+	String searches[];
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -25,13 +35,20 @@ public class Home extends Activity implements OnClickListener{
 		//LinearLayout layout = (LinearLayout) getWindow().findViewById(R.id.title_complex);
 		//layout.addView(new Button(this));
 		//layout.addView(new Button(this));
+		try {
+			loadEntries();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		setListAdapter(new ArrayAdapter<String>(Home.this, android.R.layout.simple_list_item_1, searches));
 	}
 
 
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,4 +77,70 @@ public class Home extends Activity implements OnClickListener{
 		}
 		return false;
 	}
+	
+	protected void loadEntries() throws IOException{
+		FileOutputStream fos;
+		String afterRead="";
+		//checks if any files exist, if they don't, it creates one called "saved"
+		if(fileList().length==0){
+			fos = openFileOutput("saved", Context.MODE_PRIVATE);
+		}else{
+			//reads through saved and builds a string out of it
+			FileInputStream fis = openFileInput("saved");
+			int ch;
+			StringBuffer fileContent = new StringBuffer("");
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = fis.read(buffer)) != -1) {
+			    fileContent.append(new String(buffer));
+			}
+			afterRead=fileContent.toString();
+			//reads through the built string for the number of new lines 
+			Scanner sc=new Scanner(afterRead);
+			int count=0;
+			while(sc.hasNext()){
+				if(sc.next().equals("/n")){
+					count++;
+				}
+			}
+			//builds searches array and adds the locations
+			searches=new String[count];
+			Scanner sc1=new Scanner(afterRead);
+			int i=0;
+			while(sc1.hasNext()){
+				searches[i]=sc1.nextLine();
+			}
+		}
+	}
+
+
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		
+		String location = searches[position];
+		try {
+			Class ourClass = Class.forName("com.example.maps.MainActivity");
+			Bundle basket=new Bundle();
+			basket.putString("location", location);
+			Intent ourIntent = new Intent(Home.this, ourClass);
+			ourIntent.putExtras(basket);
+			startActivity(ourIntent);
+			
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
