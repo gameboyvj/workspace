370beaa105200039037e3c92371b62bbe558181a
Vishal Jain <vkjrul3z@gmail.com>
2013-04-14 16:00:12 -0400

    Fixed force closes on Builder, other stuff
    Added edittext for entering destination
    Fixed checkboxes, they work now

--- Maps/src/com/example/maps/Builder.java
+++ Maps/src/com/example/maps/Builder.java
@@ -6,14 +6,21 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Builder extends Activity implements OnItemSelectedListener{
 	
 	TextView destination;
+	EditText address;
 	Spinner theSpinner;
 	double enactedValue=1;
+	CheckBox vib;
+	CheckBox sou;
+	int vibrate=1;
+	int sound=0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +28,17 @@ public class Builder extends Activity implements OnItemSelectedListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.builder);
 		destination=(TextView)findViewById(R.id.tvDestination);
+		address=(EditText)findViewById(R.id.etAddress);
 		theSpinner=(Spinner)findViewById(R.id.sp1);
+		vib=(CheckBox)findViewById(R.id.cbVibrate);
+		sou=(CheckBox)findViewById(R.id.cbSound);
+		if (!vib.isChecked()) {
+            vib.setChecked(true);
+        }
+		if (sou.isChecked()) {
+            sou.setChecked(false);
+        }
+		
 		// Create an ArrayAdapter using the string array and a default spinner layout
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
 		        R.array.enact_array, android.R.layout.simple_spinner_item);
@@ -31,7 +48,7 @@ public class Builder extends Activity implements OnItemSelectedListener{
 		theSpinner.setAdapter(adapter);
 	}
 
-	//what a waste to type
+	//probably a better way to do this
 	@Override
 	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
 			long arg3) {
@@ -55,5 +72,27 @@ public class Builder extends Activity implements OnItemSelectedListener{
 	public void onNothingSelected(AdapterView<?> arg0) {
 		// TODO Auto-generated method stub
 		enactedValue=1;
+	}
+	
+	public void onCheckboxClicked(View view) {
+	    // Is the view now checked?
+	    boolean checked = ((CheckBox) view).isChecked();
+	    
+	    // Check which checkbox was clicked
+	    switch(view.getId()) {
+	        case R.id.cbVibrate:
+	            if (checked)
+	                vibrate=1;
+	            else
+	                vibrate=0;
+	            break;
+	        case R.id.cbSound:
+	            if (checked)
+	                sound=1;
+	            else
+	                sound=0;
+	            break;
+	        // TODO: Veggie sandwich
+	    }
 	}
 }
